2bccda072b824d2b4caba39b1e648210de2df5a2
Shane Whithead <swhitehead@gmail.com>
2016-06-19 15:44:40 +1000

    Touch for pad button

--- DialPadTest/PadButton.swift
+++ DialPadTest/PadButton.swift
@@ -40,6 +40,13 @@ import UIKit
 		}
 	}
 	
+	@IBInspectable var textColor: UIColor = UIColor.blackColor() {
+		didSet {
+			padTextLabel.textColor = textColor
+			subTextLabel.textColor = textColor
+		}
+	}
+	
 	private var padTextLabel: UILabel = UILabel()
 	private var subTextLabel: UILabel = UILabel()
 	
@@ -56,8 +63,13 @@ import UIKit
 	}
 	
 	func setup() {
+		dimeter = 44
 		opaque = false
 		clipsToBounds = true
+		
+		padTextLabel.userInteractionEnabled = false
+		subTextLabel.userInteractionEnabled = false
+		stackView.userInteractionEnabled = false
 		
 		padText = "?"
 		borderColor = UIColor.blackColor()
@@ -105,6 +117,38 @@ import UIKit
 	public override func layoutSubviews() {
 		super.layoutSubviews()
 		layer.cornerRadius = min(bounds.width, bounds.height) / 2.0
+	}
+
+	public override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+		super.beginTrackingWithTouch(touch, withEvent: event)
+		let duration = 0.5
+		layer.backgroundColor = borderColor.CGColor
+		let animation = CABasicAnimation(keyPath: "backgroundColor")
+		animation.duration = duration
+		animation.fromValue = borderColor.CGColor
+		animation.toValue = UIColor.clearColor().CGColor
+		layer.addAnimation(animation, forKey: "backgroundColor")
+		layer.backgroundColor = UIColor.clearColor().CGColor
+
+		self.padTextLabel.textColor = UIColor.whiteColor()
+		self.subTextLabel.textColor = UIColor.whiteColor()
+		dispatch_async(dispatch_get_main_queue(), {
+			UIView.transitionWithView(self.padTextLabel, duration: duration, options: .TransitionCrossDissolve, animations: {
+				self.padTextLabel.textColor = self.textColor
+				}, completion: nil)
+			UIView.transitionWithView(self.subTextLabel, duration: duration, options: .TransitionCrossDissolve, animations: {
+				self.subTextLabel.textColor = self.textColor
+				}, completion: nil)
+		})
+		
+		self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+		
+		return true
+	}
+	
+	public override func continueTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+		super.continueTrackingWithTouch(touch, withEvent: event)
+		return false
 	}
 	
 }
